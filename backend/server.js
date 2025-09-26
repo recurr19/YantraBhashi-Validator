ec3b1ddc6533f9dd95ec28d540d8c85725f0fea4
SrushtiPekamwar <srushtimahajan65@gmail.com>
2025-09-27 00:24:30 +0530

    Add helper functions and reserved words set

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,23 @@ app.use(express.json({ limit: '2mb' }));
 app.listen(5001, () => {
   console.log('Server running on port 5001');
 });
+
+const reserved = new Set([
+  'PADAM','ANKHE','VARTTAI','ELAITHE','ALAITHE','MALLI-MALLI','CHATIMPU','CHEPPU'
+]);
+const reIdentifier = /^[A-Za-z][A-Za-z0-9_]*$/;
+const intLiteral = /^-?\d+$/;
+const stringLiteral = /^"(?:[^"\\]|\\.)*"$/;
+
+function pushError(arr, lineno, msg, suggestion = null) {
+  arr.push({ lineno, type: 'error', msg, suggestion });
+}
+function pushWarn(arr, lineno, msg, suggestion = null) {
+  arr.push({ lineno, type: 'warning', msg, suggestion });
+}
+
+// Start server
+app.listen(5001, () => {
+  console.log('Server running on port 5001');
+});
+
